0c19b489f5c08ca8d1c30ef907c1a53a088ee2fb
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:07:31 +0200

    First commit

---
+++ Frameworks/AppLovinMediationMetaAdapter.xcframework/ios-arm64/FacebookSPMAdapter.framework/Headers/FacebookSPMAdapter.h
@@ -0,0 +1,18 @@
+//
+//  FacebookSPMAdapter.h
+//  FacebookSPMAdapter
+//
+//  Created by Robert Tataru on 26.02.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FacebookSPMAdapter.
+FOUNDATION_EXPORT double FacebookSPMAdapterVersionNumber;
+
+//! Project version string for FacebookSPMAdapter.
+FOUNDATION_EXPORT const unsigned char FacebookSPMAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FacebookSPMAdapter/PublicHeader.h>
+
+
